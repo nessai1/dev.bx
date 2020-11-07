abafd463366325160e5e27699d8619698d2efe12
Daniel Verevkin <werowkin@gmail.com>
2020-11-08 00:00:08 +0200

    assertComponent.php: функция проверки соответствия ожидемых и реальных результатов работы функции. readerComponent.php: проверяемая функция. testFunction.php: реализация самих тестов.

--- homework2/testFunction.php
+++ homework2/testFunction.php
@@ -3,6 +3,7 @@
 require_once('readerComponent.php');
 require_once('assertComponent.php');
 
+/* Проверяем ввод через аргумент */
 assertComponent(true,readFromConsole('','true'), "readFromConsole('','true') = true ");
 assertComponent(false,readFromConsole('','false'), "readFromConsole('','false') = false ");
 assertComponent(null,readFromConsole('','!stop'), "readFromConsole('','!stop') = null ");
@@ -11,4 +12,7 @@ assertComponent(1,readFromConsole('','1'), "readFromConsole('','1') = 1 ");
 assertComponent('test',readFromConsole('','test'), "readFromConsole('','test') = test ");
 
 
-readFromConsole('','true');
+/* Проверяем ввод из стандартного потока */
+$q = readFromConsole('STDIN test, enter some message: ');
+echo "Your message '{$q}' \n";
+
