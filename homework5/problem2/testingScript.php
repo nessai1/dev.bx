caa87433bff6a8e0c3073e1fb5e2bbff8fd81d22
Daniel Verevkin <werowkin@gmail.com>
2020-11-24 14:50:35 +0200

    Домашняя работа #5 - Задание #2 - Ферзь и Ладья solveScript.php: файл содержащий классы шахматных фигур (Ладья и Ферзь) testingScript.php: файл с функциями тестирования шахматных фигур demoScript.php: файл с функцией демонстрации работы решения, принимающей данные из консоли index.php: исполнение тестов и демонстрационной функции

--- homework5/problem2/testingScript.php
+++ homework5/problem2/testingScript.php
@@ -16,10 +16,16 @@ function queen_TEST()
     $queen = new Queen(1,1);
     assertComponent(true,$queen->validateMove(3,3), "[Test 1]: перемещение с (1,1) на (3,3) возможно ");
     assertComponent(true,$queen->validateMove(4,1), "[Test 2]: перемещение с (1,1) на (4,1) возможно ");
-    assertComponent(false,$queen->validateMove(9,9), "[Test 3]: перемещение с (1,1) на (9,9) невозможно т.к. ферзь выйдет за доску ");
+    assertComponent(true,$queen->validateMove(1,4), "[Test 3]: перемещение с (1,1) на (1,4) возможно ");
+    assertComponent(false,$queen->validateMove(9,9), "[Test 4]: перемещение с (1,1) на (9,9) невозможно т.к. ферзь выйдет за доску ");
     $queen->setCoords(3, 2);
-    assertComponent(true, $queen->validateMove(2,1), "[Test 4]: перемещение с (3,2) на (2,1) возможно ");
-    assertComponent(false, $queen->validateMove(5,3), "[Test 5]: перемещение с (3,2) на (5,3) невозможно ");
+    assertComponent(true, $queen->validateMove(2,1), "[Test 5]: перемещение с (3,2) на (2,1) возможно ");
+    assertComponent(true, $queen->validateMove(3,1), "[Test 6]: перемещение с (3,2) на (3,1) возможно ");
+    assertComponent(true, $queen->validateMove(2,2), "[Test 7]: перемещение с (3,2) на (2,2) возможно ");
+    assertComponent(true, $queen->validateMove(4,1), "[Test 8]: перемещение с (3,2) на (4,1) возможно ");
+    assertComponent(true, $queen->validateMove(2,3), "[Test 9]: перемещение с (3,2) на (2,3) возможно ");
+    assertComponent(true, $queen->validateMove(4,3), "[Test 10]: перемещение с (3,2) на (4,3) возможно ");
+    assertComponent(false, $queen->validateMove(5,3), "[Test 11]: перемещение с (3,2) на (5,3) невозможно ");
     unset($queen);
 }
 
@@ -30,10 +36,12 @@ function rook_TEST()
 
     $rook = new Rook(1,1);
     assertComponent(true, $rook->validateMove(5,1), "[Test 1]: перемещение с (1,1) на (5,1) возможно ");
-    assertComponent(false, $rook->validateMove(5,5), "[Test 2]: перемещение с (1,1) на (5,5) невозможно ");
-    assertComponent(false, $rook->validateMove(9,1), "[Test 3]: перемещение с (1,1) на (9,1) невозможно т.к. ладья выйдет за доску ");
+    assertComponent(true, $rook->validateMove(1,5), "[Test 2]: перемещение с (1,1) на (1,5) возможно ");
+    assertComponent(false, $rook->validateMove(5,5), "[Test 3]: перемещение с (1,1) на (5,5) невозможно ");
+    assertComponent(false, $rook->validateMove(9,1), "[Test 4]: перемещение с (1,1) на (9,1) невозможно т.к. ладья выйдет за доску ");
     $rook->setCoords(3,3);
-    assertComponent(true, $rook->validateMove(2,3), "[Test 4]: перемещение с (3,3) на (2,3) возможно ");
+    assertComponent(true, $rook->validateMove(2,3), "[Test 5]: перемещение с (3,3) на (2,3) возможно ");
+    assertComponent(true, $rook->validateMove(3,2), "[Test 6]: перемещение с (3,3) на (3,2) возможно ");
     unset($rook);
 }
 
