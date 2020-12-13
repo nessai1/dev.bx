6d600e8e60da438dd9ba69dd0b4487645fd646d2
Daniel Verevkin <werowkin@gmail.com>
2020-12-13 13:58:26 +0200

    Домашняя работа #9 Исправление ошибок. Добавил в выборку книг из таблицы book константу 1 на место столбца STORE_ID Избавился от дефолтного значения столбца STORE_ID при создании таблицы book_store

--- homework9/migration.sql
+++ homework9/migration.sql
@@ -8,17 +8,14 @@ INSERT INTO `store` (`CITY`) VALUES ('Калининград'), ('Черняхо
 
 CREATE TABLE `book_store` (
   `BOOK_ID` int NOT NULL REFERENCES book(ID),
-  `STORE_ID` int DEFAULT 1 REFERENCES store(ID),
+  `STORE_ID` int NOT NULL REFERENCES store(ID),
   `PRICE` DECIMAL(10, 2),
   `QUANTITY` int unsigned default 0,
   PRIMARY KEY (BOOK_ID, STORE_ID)
 );
 
-INSERT INTO `book_store` (`BOOK_ID`, `PRICE`, `QUANTITY`)
-SELECT `ID`, `PRICE`, `QUANTITY` FROM `book`;
-
-ALTER TABLE `book_store`
-MODIFY `STORE_ID` int NOT NULL;
+INSERT INTO `book_store` (`STORE_ID`,`BOOK_ID`, `PRICE`, `QUANTITY`)
+SELECT 1, `ID`, `PRICE`, `QUANTITY` FROM `book`;
 
 ALTER TABLE `book`
 DROP COLUMN `QUANTITY`, DROP COLUMN `PRICE`;
